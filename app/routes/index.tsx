6a2e919cef6838ab79ec7d6f87f41afb43a884e8
Lukas Cornille <lukas.cornille@inthepocket.mobi>
2022-03-22 14:13:00 +0100

    feat: add initial markup

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,5 +1,32 @@
+import { Link } from 'remix';
+
 const Index = () => {
-  return <main></main>;
+  return (
+    <main>
+      <form method="post">
+        <h2>Subscribe!</h2>
+        <p>Don't miss any of the action!</p>
+        <fieldset>
+          <input
+            aria-label="Email address"
+            aria-describedby="error-message"
+            type="email"
+            name="email"
+            placeholder="you@example.com"
+          />
+          <button type="submit">Subscribe</button>
+        </fieldset>
+
+        <p id="error-message" />
+      </form>
+
+      <div style={{ display: 'none' }}>
+        <h2>You're subscribed!</h2>
+        <p>Please check your email to confirm your subscription.</p>
+        <Link to=".">Start over</Link>
+      </div>
+    </main>
+  );
 };
 
 export default Index;
